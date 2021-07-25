0493c0a010b00e7111ddc8d03857f4e38624c242
Ahbab Shahin Chowdhury <85829019+ahbabshahin@users.noreply.github.com>
2021-07-25 10:01:39 +0600

    Add files via upload

---
+++ Login System/partials/_database.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// if($conn){
+
+//    echo' <script>
+//    alert("Connection Successful!");
+//    </script>';
+// }
+
+// else{
+//     echo mysqli_error($conn);
+// }
+
+?>
---
+++ Login System/partials/_navbar.php
@@ -0,0 +1,55 @@
+<?php
+
+//session_start();
+$loggedin = false;
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+{
+  $loggedin = true;
+}
+
+echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+<div class="container-fluid">
+
+  <a class="navbar-brand" href="index.php">Yelena</a>
+
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <span class="navbar-toggler-icon"></span>
+
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">';
+
+    if(!$loggedin){
+     echo '<li class="nav-item">
+        <a class="nav-link" aria-current="page" href="signup.php">Sign up</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>';
+    }
+
+    if($loggedin){
+     echo' <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>';
+    }
+   echo' </ul>
+    
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+
+    </form>
+
+  </div>
+
+</div>
+
+</nav>';
+
+?>
